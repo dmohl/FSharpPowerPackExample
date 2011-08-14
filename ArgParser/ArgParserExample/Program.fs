041ec7f9b0df93b6ccb342d2499d5f10d9dcb8de
Dan Mohl <dmohl@yahoo.com>
2011-08-13 20:24:48 -0500

    Created

---
+++ ArgParser/ArgParserExample/Program.fs
@@ -0,0 +1,20 @@
+﻿// From http://laurent.le-brun.eu/site/index.php/2010/06/08/55-fsharp-getopt-to-parse-the-command-line
+
+open System
+open Microsoft.FSharp.Text
+
+let compile s = printfn "Compiling %s..." s
+ 
+let outputName = ref "a.out"
+let verbose = ref false
+let warningLevel = ref 0
+let specs =
+    ["-o", ArgType.String (fun s -> outputName := s), "Name of the output"
+     "-v", ArgType.Set verbose, "Display additional information"
+     "--warn", ArgType.Int (fun i -> warningLevel := i), "Set warning level"
+     "--", ArgType.Rest compile, "Stop parsing command line"
+    ] |> List.map (fun (sh, ty, desc) -> ArgInfo(sh, ty, desc))
+
+let () = 
+    ArgParser.Parse(specs, compile)
+
---
+++ ArgParser/ArgParserExample/readme.txt
@@ -0,0 +1,3 @@
+﻿In a command console, launch the ArgParserExample.exe with the --help option.
+
+Example: ArgParserExample.exe -help
