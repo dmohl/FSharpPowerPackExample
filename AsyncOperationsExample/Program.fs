684a03a184b56a79d316caa8f96fd59974489490
Dan Mohl <dmohl@yahoo.com>
2011-08-14 10:49:35 -0500

    Added several more examples

---
+++ AsyncOperationsExample/Program.fs
@@ -0,0 +1,25 @@
+﻿// This sample is a modified version of one of the unit tests found in ControlTests.fs 
+// at http://fsharppowerpack.codeplex.com/SourceControl/changeset/view/64420#685261
+open System.IO
+
+async {
+    async {
+        let buffer = "F# is fun!"B
+        use! is = File.AsyncOpenWrite "test.txt"
+        do! is.AsyncWrite(buffer, 0, buffer.Length) 
+        printfn "File written" } 
+    |> Async.RunSynchronously
+
+    async { 
+        let buffer = Array.zeroCreate<byte>(7)
+        use! is = File.AsyncOpenRead "test.txt"
+        let! count = is.AsyncRead(buffer, 0, 7)
+        printfn "File read"
+        return count, buffer }
+    |> Async.RunSynchronously |> ignore
+} 
+|> Async.Start
+
+printfn "Starting to write then read a file asynchronously..."
+
+System.Console.Read()
---
+++ AsyncStreamReaderExample/Program.fs
@@ -0,0 +1,31 @@
+﻿open System
+open System.IO
+open System.Text
+
+let text = [ new String([|for i in 1..1022 do yield 'x'|])
+             new String([|for i in 1..1022 do yield 'y'|])
+             new String([|for i in 1..1022 do yield 'z'|]) ].ToString()  
+
+use stream = new MemoryStream()
+stream.Write(Encoding.UTF8.GetBytes(text), 0, text.Length)
+stream.Position <- 0L
+let reader = new AsyncStreamReader(stream, Encoding.UTF8)          
+async { let rec readAllChars () =
+            async {
+                let! eof = reader.EndOfStream
+                if not eof then
+                    let! character = reader.Read()
+                    do printf "%s" (character.ToString())
+                    return! readAllChars () 
+                else 
+                    printfn "%sRead complete" Environment.NewLine           
+            }
+        do! readAllChars ()
+} 
+|> Async.Start
+
+printfn "Started reading the memory stream"
+
+Console.Read()
+    
+                 
